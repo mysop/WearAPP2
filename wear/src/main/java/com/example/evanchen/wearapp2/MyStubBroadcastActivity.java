20d1bba4343189c92ffc41d206a5a5ea7dbf6de6
fatguy <tzyshing9@gmail.com>
2016-01-07 16:20:08 +0800

    0107

---
+++ wear/src/main/java/com/example/evanchen/wearapp2/MyStubBroadcastActivity.java
@@ -0,0 +1,21 @@
+package com.example.evanchen.wearapp2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("com.example.evanchen.wearapp2.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
